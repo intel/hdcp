3fa64e00dd2864b59e6dd6f9d410313ae719d1b4
kondasai <saikishore.konda@intel.com>
2019-02-11 10:47:28 +0000

    removing not required functions used by hdcp module from global_wayland header file
    Signed-off-by: kondasai <saikishore.konda@intel.com>

--- daemon/global_wayland.h
+++ daemon/global_wayland.h
@@ -18,24 +18,10 @@
 #ifndef _GLOBAL_WAYLAND_H_
 #define _GLOBAL_WAYLAND_H_
 
-/* For GEM */
-#include <libdrm/intel_bufmgr.h>
-#include <xf86drm.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
 
 #include <wayland-client.h>
 #include <wayland-egl.h>
 
-
-enum _caps {
-	SUPPORT_NONE,
-	SUPPORT_DISP_WL,
-};
-
-#define MAKE_LEVEL(lvl, caps) (lvl<<4 | caps)
 
 struct crtc {
 	struct ias_crtc *ias_crtc;
@@ -61,37 +47,10 @@ class global_wl {
 		virtual void deinit();
 		virtual void dispatch_pending();
 		virtual void add_reg();
-		virtual void registry_handle_global(void *data,
-				struct wl_registry *registry, uint32_t name,
-				const char *interface, uint32_t version);
-		virtual void registry_handle_global_remove(void *data,
-				struct wl_registry *registry, uint32_t name);
+		/* adding dummy functions to map with actual global_wl class */
+		virtual void dummy1();
+		virtual void dummy2();
 		virtual bool set_content_protection(int crtc, int cp);
-		static void xdg_shell_ping(void *data, struct zxdg_shell_v6 *shell,
-				uint32_t serial);
-		static void modelist_event(void *data,
-				struct ias_crtc *ias_crtc,
-				uint32_t mode_number,
-				uint32_t width, uint32_t height, uint32_t refresh);
-		static void gamma_event(void *data,
-				struct ias_crtc *ias_crtc,
-				uint32_t red,
-				uint32_t green,
-				uint32_t blue);
-		static void contrast_event(void *data,
-				struct ias_crtc *ias_crtc,
-				uint32_t red,
-				uint32_t green,
-				uint32_t blue);
-		static void brightness_event(void *data,
-				struct ias_crtc *ias_crtc,
-				uint32_t red,
-				uint32_t green,
-				uint32_t blue);
-		static void id_event(void *data,
-				struct ias_crtc *ias_crtc,
-				uint32_t id);
-		static void cp_enabled_event(void *data, struct ias_crtc *ias_crtc);
 };
 
 #endif
